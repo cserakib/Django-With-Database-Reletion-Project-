7a3774094c6b869827ed8c90be2de8a7b0bd965e
rakib <cserakib13@gmail.com>
2020-07-16 23:58:03 +0600

    Created a model for Quote_category and quote , also add admin

--- Quote_pro/Main_apps/models.py
+++ Quote_pro/Main_apps/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class QuoteCategory(models.Model):
+    title = models.CharField(max_length= 50)
+
+
+class Quote(models.Model):
+    quote = models.TextField()
+    Author = models.CharField(max_length= 50)
+
+    quote_category = models.ForeignKey(
+        'QuoteCategory', on_delete = models.CASCADE
+    )
